42c7fcab87ff96efa48873f7c3c2a2a103cb6412
jmelvin <john@blaqsmith.com>
2024-09-22 09:30:11 -0500

    Establishing basic repository

---
+++ Assets/Scripts/NewScriptFor you.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScriptForyou : MonoBehaviour
+{
+	//~~~Definitions~~~
+
+	//~~~Variables~~~
+	//Serialized
+
+	//Non-Serialized
+
+	//~~~Accessors~~~
+
+	//~~~Unity Functions~~~
+
+	void Start()
+	{
+		
+	}
+	
+	//~~~Runtime Functions~~~
+	
+	//~~~Callback Functions~~~
+
+	//~~~Editor Functions~~~
+	#if UNITY_EDITOR
+	
+	#endif
+	
+}
